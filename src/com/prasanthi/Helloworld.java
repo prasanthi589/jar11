c54b07dbf7bdd786f06022e60e2e557a09e525d2
prasanthi <prasanthi589>
2017-10-09 20:35:00 +0530

    polling

--- src/com/prasanthi/Helloworld.java
+++ src/com/prasanthi/Helloworld.java
@@ -7,7 +7,7 @@ public class Helloworld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hjdgah");
+		System.out.println("hello");
 
 	}
 
